b8393c73220a32722b559b5ce8a12809f8d5444e
CHOUBEY SHIVAM SUNIL <2022.shivam.choubey@ves.ac.in>
2026-04-23 09:22:09 +0530

    Update __init__.py

--- dlexams/__init__.py
+++ dlexams/__init__.py
@@ -1,2 +1,10 @@
 from .experiments import show, codes
 print("✅ dlexams loaded! Use show() to list all experiments or show('exp1') to run one.")
+print("exp1  ->  1. MLP XOR
+  exp2  ->  2. Train MLP on MNIST & Plot Loss Function
+  exp3  ->  3. Compare tanh vs Leaky ReLU on Fashion MNIST & Plot
+  exp4  ->  4. Learning Rate Schedule & Early Stopping on CIFAR-10
+  exp5  ->  5. Batch Normalization & Dropout on SVHN
+  exp6  ->  6. Denoising Autoencoder on MNIST & Plot Curves
+  exp7  ->  7. CNN on CelebA Dataset
+  exp8  ->  8. Optimize MLP for Fashion MNIST")
